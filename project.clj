c67c7fa02ede0e4b3b92e00f6442d253ac377ad8
Stian Soiland-Reyes <stain@apache.org>
2016-12-03 16:42:38 +0000

    use parse-ez?

--- project.clj
+++ project.clj
@@ -1,9 +1,12 @@
-(defproject eolang "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject eolang "0.0.1-SNAPSHOT"
+  :description "Experimental eolang parser for Clojure"
   :url "https://github.com/stain/eolang-clj"
   :license {:name "The MIT License"
             :url "https://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [
+    [org.clojure/clojure "1.8.0"]
+    [protoflex/parse-ez "0.4.2"]
+  ]
   :main ^:skip-aot eolang.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
